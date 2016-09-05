807a5b78226855a34fa2f877c5934d9ce3c0478a
Matt McGrath <mjm3853@yahoo.com>
2016-09-04 23:43:59 -0400

    added a few lift types

--- src/app/shared/lift.store.ts
+++ src/app/shared/lift.store.ts
@@ -27,7 +27,10 @@ export class WorkoutStore {
         "Push Ups",
         "Pull Ups",
         "Squats",
-        "Deadlifts"
+        "Deadlifts",
+        "Kettlebell Upright Row",
+        "Kettlebell Deadlifts",
+        "Dumbbell One Arm Row"
     ]
 
     constructor() {
